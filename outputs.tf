c65d04072fe3dd2e891a342045ac4894ae458340
Daniel Allen <dan@aeternagroup.net>
2026-07-12 16:00:53 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,7 @@
+output "bot_channel_facebooks_id" {
+  description = "Map of id values across all bot_channel_facebooks, keyed the same as var.bot_channel_facebooks"
+  value       = { for k, v in azurerm_bot_channel_facebook.bot_channel_facebooks : k => v.id }
+}
 output "bot_channel_facebooks_bot_name" {
   description = "Map of bot_name values across all bot_channel_facebooks, keyed the same as var.bot_channel_facebooks"
   value       = { for k, v in azurerm_bot_channel_facebook.bot_channel_facebooks : k => v.bot_name }
